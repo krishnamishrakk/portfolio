31a133678789da769464c3b12a649cd01d582af2
krishnamishrakk <krishm9294@gmail.com>
2022-09-19 19:15:28 +0530

    projects

--- src/Detail.js
+++ src/Detail.js
@@ -6,6 +6,6 @@ import todo from './Image/Todo.png'
 export const details = [
     {heading : "Blog Project", dis : "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content", img : blog, tech : "HTML, CSS, JavaScript, React, Node", github : "https://github.com/krishnamishrakk/reactpro", preview : "https://62cd756e66d54e1649918ae6--taupe-scone-045e74.netlify.app//"},
     {heading : "Digital  Clock", dis : "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content", img : clock, tech : "HTML, CSS, JavaScript", github : "https://github.com/krishnamishrakk/digitalclockjs", preview : "https://krishnamishrakk.github.io/digitalclockjs/dig.html"},
-    {heading : "Todo List", dis : "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content", img : todo, tech : "HTML, CSS, JavaScript, React", github : "https://github.com/krishnamishrakk/todojs", preview : "https://krishnamishrakk.github.io/todojs/todo.html/"},
+    {heading : "Todo List", dis : "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content", img : todo, tech : "HTML, CSS, JavaScript, React", github : "https://github.com/krishnamishrakk/todo-mobile", preview : "https://krishnamishrakk.github.io/todo-mobile//"},
    
 ]
